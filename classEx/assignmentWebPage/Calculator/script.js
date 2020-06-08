c7d17b3f710165b67f0d3be84ab63f3a907bcb32
Bon Vo <63612593+Bonvo1998@users.noreply.github.com>
2020-06-08 12:47:30 -0700

    Add files via upload

---
+++ classEx/assignmentWebPage/Calculator/script.js
@@ -0,0 +1,54 @@
+var val1, val2;
+function loadDisplay(num) {
+    document.getElementById('display2').value += num.value.toString();
+}
+function delFromRight () {
+    document.getElementById('display2').value = document.getElementById('display2').value.slice(0,-1);
+}
+function clearDisplay() {
+    document.getElementById('display2').value = "";
+}
+function setOperator(trg) {
+    val1 = document.getElementById('display2').value + " " + trg.value;
+    document.getElementById('display1').innerHTML = val1;
+    document.getElementById('display2').value = "";
+}
+function doMath() {
+    // alert('reached');
+    val2 = document.getElementById('display2').value;
+    var result;
+    switch(val1.slice(-1)){
+        case '+':
+            result = parseInt(val1) + parseInt(val2);
+            break;
+        case '-':
+            result = parseInt(val1) - parseInt(val2);
+            break;
+        case '*':
+            result = parseInt(val1) * parseInt(val2);
+            break;
+        case '/':
+            result = parseInt(val1) / parseInt(val2);
+            break;
+        // case 'sqrt':
+        //    // dp square root math
+        //    break;
+        // case 'pow':
+        //    // do exponent math
+        //    break;
+    }
+    // alert(result);
+    document.getElementById('display1').innerHTML = "";
+    document.getElementById('display2').value = result;
+    val1 = null;
+    val2 = null;
+    result = null;
+}
+// function specialCase(){
+//    if(sqrt){
+//        // do square root math
+//    }else{
+
+//       // do exponent
+//    }
+// }
---
+++ classEx/assignmentWebPage/index.js
@@ -0,0 +1,34 @@
+
+function changeLinkText(lnk) {
+ 
+    if(lnk == "ce") {
+        document.getElementById('divLeftDefault').style.display = "none";
+        document.getElementById('divLeft_CELinks').style.display = "block";
+        document.getElementById('divLeft_ALinks').style.display = "none";
+        document.getElementById('divRight').style.display = "none";
+        document.getElementById('ls_Heading').innerHTML = "Class Exercises";
+        document.getElementById('rs_Heading').innerHTML = "This is where you can find my class exercises.";
+    }else if (lnk == 'a'){
+        document.getElementById('divLeftDefault').style.display = "none";
+        document.getElementById('divLeft_CELinks').style.display = "none";
+        document.getElementById('divLeft_ALinks').style.display = "block";
+        document.getElementById('divRight').style.display = "none";
+        document.getElementById('ls_Heading').innerHTML = "Assignments";
+        document.getElementById('rs_Heading').innerHTML = "This is where you can find my class assignments.";
+    
+    }else{
+        document.getElementById('divLeftDefault').style.display = "block";
+        document.getElementById('divLeft_CELinks').style.display = "none";
+        document.getElementById('divLeft_ALinks').style.display = "none";
+        document.getElementById('divRight').style.display = "none";
+    }
+    
+    document.getElementById('divRight').innerHTML = 'Content for the "Link 1" link.';
+    
+    document.getElementById('divRight').style.color = 'red';
+    }
+    
+    function changeDivRightText(txt) {
+          
+    document.getElementById('rs_Heading').innerHTML = txt;
+    }
